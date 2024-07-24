7cb3f55a47e9fdd76b968da1ef1e4ab00b4c51a3
mukeshkuiry <mukeshkk3162@gmail.com>
2024-07-25 00:16:24 +0530

    fix: infra layout mobile

--- components/Benefits.tsx
+++ components/Benefits.tsx
@@ -11,13 +11,13 @@ const Benefits: React.FC = () => {
         <span className="text-[#21dbd8]">Architecture</span>
       </h1>
       <motion.div
-        className="border p-2 w-[80vw] md:w-[20vw] md:h-[12vh] rounded-3xl"
+        className="border p-2 w-[80vw] md:w-[20vw] h-[12vh] md:rounded-3xl"
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
         viewport={{ once: false }}
       >
-        <div className="border border-dashed rounded-3xl p-4 md:h-[12vh] w-[20vw] bg-[#1F1F1C] flex flex-col justify-center items-center">
+        <div className="border border-dashed md:rounded-3xl p-4 h-[12vh] w-[80vw] md:w-[20vw] bg-[#1F1F1C] flex flex-col justify-center items-center">
           <h1 className="text-3xl font-semibold text-[#ff9933]">
             Gen-AI Employees
           </h1>
@@ -27,7 +27,7 @@ const Benefits: React.FC = () => {
       <div className="h-12 w-3/5 border-x border-t rounded-t-full border-dashed border-gray-400"></div>
       <div className="flex flex-col md:flex-row gap-8 justify-center items-center">
         <motion.div
-          className="p-2 w-[80vw] md:w-[12vw] h-[15vh] md:h-[8vh] flex justify-center items-center"
+          className="p-2 w-[80vw] md:w-[12vw] h-0 md:h-[8vh] flex justify-center items-center"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
@@ -36,13 +36,13 @@ const Benefits: React.FC = () => {
           <h1 className="text-lg text-gray-400 mt-5"></h1>
         </motion.div>
         <motion.div
-          className="border p-2 w-[80vw] md:w-[12vw] h-[15vh] md:h-[8vh] rounded-tl-3xl"
+          className="border p-2 w-[80vw] md:w-[12vw] h-[15vh] md:h-[8vh] md:rounded-tl-3xl"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
           viewport={{ once: false }}
         >
-          <div className="border border-dashed rounded-tl-3xl p-4 md:p-8 w-[80vw] md:w-[12vw] h-[15vh] md:h-[8vh] bg-[#1F1F1C] flex flex-col justify-center items-center">
+          <div className="border border-dashed md:rounded-tl-3xl p-4 md:p-8 w-[80vw] md:w-[12vw] h-[15vh] md:h-[8vh] bg-[#1F1F1C] flex flex-col justify-center items-center">
             <h1 className="text-2xl font-semibold text-[#21dbd8]">Sales</h1>
           </div>
         </motion.div>
@@ -82,18 +82,18 @@ const Benefits: React.FC = () => {
           </div>
         </motion.div>
         <motion.div
-          className="border p-2 w-[80vw] md:w-[12vw] h-[15vh] md:h-[8vh] rounded-tr-3xl"
+          className="border p-2 w-[80vw] md:w-[12vw] h-[15vh] md:h-[8vh] md:rounded-tr-3xl"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
           viewport={{ once: false }}
         >
-          <div className="border border-dashed rounded-tr-3xl p-4 md:p-8 w-[80vw] md:w-[12vw] h-[15vh] md:h-[8vh] bg-[#1F1F1C] flex flex-col justify-center items-center">
+          <div className="border border-dashed md:rounded-tr-3xl p-4 md:p-8 w-[80vw] md:w-[12vw] h-[15vh] md:h-[8vh] bg-[#1F1F1C] flex flex-col justify-center items-center">
             <h1 className="text-2xl font-semibold text-[#21dbd8]">Analytics</h1>
           </div>
         </motion.div>
         <motion.div
-          className="p-2 w-[80vw] md:w-[12vw] h-[15vh] md:h-[8vh] flex justify-center items-center -ml-10"
+          className="p-2 w-[80vw] md:w-[12vw] md:h-[15vh] md:h-[8vh] flex justify-center items-center -ml-10"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
@@ -109,13 +109,13 @@ const Benefits: React.FC = () => {
       </div>
       <div className="w-1 border-l border-dashed h-8"></div>
       <motion.div
-        className="border p-2 w-[80vw] md:w-[25vw] md:h-[16vh] rounded-3xl"
+        className="border p-2 w-[80vw] md:w-[25vw] md:h-[16vh] md:rounded-3xl"
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
         viewport={{ once: false }}
       >
-        <div className="border border-dashed rounded-3xl p-4 md:h-[16vh] w-[25vw] bg-[#1F1F1C] flex flex-col justify-center items-center">
+        <div className="border border-dashed md:rounded-3xl p-4 md:h-[16vh] w-[80vw] md:w-[25vw] bg-[#1F1F1C] flex flex-col justify-center items-center">
           <h1 className="text-4xl font-semibold text-[#ff9933]">
             Application Layer
           </h1>
@@ -123,13 +123,13 @@ const Benefits: React.FC = () => {
       </motion.div>
       <h1 className="text-3xl text-[#ffffffbc] my-6">+</h1>
       <motion.div
-        className="border p-2 w-[80vw] md:w-[18vw] md:h-[10vh] rounded-3xl "
+        className="border p-2 w-[80vw] md:w-[18vw] md:h-[10vh] md:rounded-3xl "
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
         viewport={{ once: false }}
       >
-        <div className="border border-dashed rounded-3xl p-4 md:h-[10vh] w-[18vw] bg-[#1F1F1C] flex flex-col justify-center items-center">
+        <div className="border border-dashed md:rounded-3xl p-4 md:h-[10vh] w-[80vw] md:w-[18vw] bg-[#1F1F1C] flex flex-col justify-center items-center">
           <h1 className="text-2xl font-semibold text-[#21dbd8] text-center">
             Security + Governance Layer
           </h1>
@@ -138,13 +138,13 @@ const Benefits: React.FC = () => {
       <h1 className="text-3xl text-[#ffffffbc] my-6">+</h1>
 
       <motion.div
-        className="border p-2 w-[80vw] md:w-[25vw] md:h-[16vh] rounded-3xl"
+        className="border p-2 w-[80vw] md:w-[25vw] h-[16vh] md:rounded-3xl"
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
         viewport={{ once: false }}
       >
-        <div className="border border-dashed rounded-3xl p-4 md:h-[16vh] w-[25vw] bg-[#1F1F1C] flex flex-col justify-center items-center">
+        <div className="border border-dashed md:rounded-3xl p-4 h-[16vh] w-[80vw] md:w-[25vw] bg-[#1F1F1C] flex flex-col justify-center items-center">
           <h1 className="text-4xl font-semibold text-[#ff9933]">
             Infrastructure Layer
           </h1>
