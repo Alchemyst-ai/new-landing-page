7c031f32f69377f859bf824070f5b1b31fc4ce9b
Anuran Roy <anuranroy02@gmail.com>
2024-08-21 20:13:47 +0530

    [MODIFY] Uncomment 100 Unicorns logo

--- app/constants/trustedby.ts
+++ app/constants/trustedby.ts
@@ -2,9 +2,9 @@ export const trustedby = [
   {
     image: "/trustedby/inflection.png",
   },
-  // {
-  //   image: "/trustedby/100unicorn.png",
-  // },
+  {
+    image: "/trustedby/100unicorn.png",
+  },
   {
     image: "/trustedby/earlyseed.png",
   },
