0580802c2c57093dbd05e2caf87c068a4056ff95
Debayan Pradhan <pradhandebayan@gmail.com>
2024-10-19 18:24:17 +0530

    minor styling fix in team card

--- components/home/TeamMemberCard.tsx
+++ components/home/TeamMemberCard.tsx
@@ -11,7 +11,7 @@ interface TeamMemberCardProps {
 const TeamMemberCard: React.FC<TeamMemberCardProps> = ({ member }) => {
   return (
     <div className="flex flex-col items-center justify-center p-4">
-      <div className="relative flex flex-col items-end justify-end h-[37vh] overflow-hidden rounded-lg bg-gradient-radial to-[#04022eed] via-[#15107aed] from-[#353B92] group ">
+      <div className="relative flex flex-col items-end justify-end max-h-min overflow-hidden rounded-lg bg-gradient-radial to-[#04022eed] via-[#15107aed] from-[#353B92] group "> 
         <Image src={member.image} alt={member.name} width={400} height={500} />
         <HoverOverlay member={member} />
         <BottomOverlay member={member} />
