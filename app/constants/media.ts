234ecc0f3f0873a34928dd5c1f9410eb36f9cdbf
Debayan Pradhan <pradhandebayan@gmail.com>
2024-10-19 17:20:05 +0530

    [ADD] coverage changes

--- app/constants/media.ts
+++ app/constants/media.ts
@@ -1,38 +1,111 @@
+// import { MediaItem } from "../types/media";
+
 import { MediaItem } from "../types/media";
 
-const mediaItems: MediaItem[] = [
-  {
-    title: "BITS Pilani’s Conquest — 5 B2B startups make it to the 2024 cohort",
-    date: "Aug 6, 2024",
-    description:
-      "Redundancy is a key issue across teams in startups, whether it be redundant sales prospect loops or feedback loops for marketing campaigns. Most of these can be avoided by structuring data, instead of siloing it across the company knowledge base.",
-    link: "https://theprint.in/india/bits-pilanis-conquest-5-b2b-startups-make-it-to-the-2024-cohort/2208870/",
-    imgSrc: "/media/conquest.png",
-  },
-  {
-    title: "29 top Deep Tech companies and startups in India in August 2024",
-    date: "Aug 1, 2024",
-    description:
-      "Alchemyst AI is building the next-gen AI digital employees for the world. These employees can automate entire workflows in go-to-market teams. Our digital employees integrate into workspaces in minutes and start working. Our first product, Maya, is an AI employee which has 50x the efficiency a human SDR would have but costs over 90% less.",
-    link: "#",
-    imgSrc: "/media/f6s.png",
-  },
-  {
-    title: "National Winner of Navishkar 2024",
-    date: "July 30, 2024",
-    description:
-      "Navachar Research Incubation and Innovation Council, in collaboration with XLRI, presents NAVISHKAR 2024, India’s largest young innovator initiative. The event features groundbreaking ideas from young innovators in technology, healthcare, sustainability, and education.",
-    link: "https://startupnews.fyi/event/navishkar-2024-jharkhand-july-29/",
-    imgSrc: "/media/nav.png",
-  },
-  {
-    title: "Top 10 Startups in Karnataka",
-    date: "Jun 29 , 2024",
-    description:
-      "Startup applications accepted at a state-level, spanning 20+ states, with 2000+ startups applying for the program. Shortlisted 8-10 startups for the finals of each state.",
-    link: "https://www.headstart.in/bharat-pitchathon",
-    imgSrc: "/media/bharat.png",
-  },
-];
+// const mediaItems: MediaItem[] = [
+//   {
+//     title: "BITS Pilani’s Conquest — 5 B2B startups make it to the 2024 cohort",
+//     date: "Aug 6, 2024",
+//     description:
+//       "Redundancy is a key issue across teams in startups, whether it be redundant sales prospect loops or feedback loops for marketing campaigns. Most of these can be avoided by structuring data, instead of siloing it across the company knowledge base.",
+//     link: "https://theprint.in/india/bits-pilanis-conquest-5-b2b-startups-make-it-to-the-2024-cohort/2208870/",
+//     imgSrc: "/media/conquest.png",
+//   },
+//   {
+//     title: "29 top Deep Tech companies and startups in India in August 2024",
+//     date: "Aug 1, 2024",
+//     description:
+//       "Alchemyst AI is building the next-gen AI digital employees for the world. These employees can automate entire workflows in go-to-market teams. Our digital employees integrate into workspaces in minutes and start working. Our first product, Maya, is an AI employee which has 50x the efficiency a human SDR would have but costs over 90% less.",
+//     link: "#",
+//     imgSrc: "/media/f6s.png",
+//   },
+//   {
+//     title: "National Winner of Navishkar 2024",
+//     date: "July 30, 2024",
+//     description:
+//       "Navachar Research Incubation and Innovation Council, in collaboration with XLRI, presents NAVISHKAR 2024, India’s largest young innovator initiative. The event features groundbreaking ideas from young innovators in technology, healthcare, sustainability, and education.",
+//     link: "https://startupnews.fyi/event/navishkar-2024-jharkhand-july-29/",
+//     imgSrc: "/media/nav.png",
+//   },
+//   {
+//     title: "Top 10 Startups in Karnataka",
+//     date: "Jun 29 , 2024",
+//     description:
+//       "Startup applications accepted at a state-level, spanning 20+ states, with 2000+ startups applying for the program. Shortlisted 8-10 startups for the finals of each state.",
+//     link: "https://www.headstart.in/bharat-pitchathon",
+//     imgSrc: "/media/bharat.png",
+//   },
+// ];
 
-export default mediaItems;
+// export default mediaItems;
+
+
+const mediaTimes : MediaItem[] = [
+{
+  title: "BITS Pilani’s Conquest — 5 B2B startups make it to the 2024 cohort",
+  date: "Aug 6, 2024",
+  description:
+    "Redundancy is a key issue across teams in startups, whether it be redundant sales prospect loops or feedback loops for marketing campaigns. Most of these can be avoided by structuring data, instead of siloing it across the company knowledge base.",
+  link: "https://theprint.in/india/bits-pilanis-conquest-5-b2b-startups-make-it-to-the-2024-cohort/2208870/",
+  imgSrc: "/media/conquest.png",
+},
+{
+  title: "29 top Deep Tech companies and startups in India in August 2024",
+  date: "Aug 1, 2024",
+  description:
+    "Alchemyst AI is building the next-gen AI digital employees for the world. These employees can automate entire workflows in go-to-market teams. Our digital employees integrate into workspaces in minutes and start working. Our first product, Maya, is an AI employee which has 50x the efficiency a human SDR would have but costs over 90% less.",
+  link: "https://www.financialexpress.com/business/brandwagon-alchemyst-ai-raises-300k-to-drive-ai-powered-digital-workforce-solutions-3641283/",
+  imgSrc: "/media/f6s.png",
+},
+{
+  title: "National Winner of Navishkar 2024",
+  date: "July 30, 2024",
+  description:
+    "Navachar Research Incubation and Innovation Council, in collaboration with XLRI, presents NAVISHKAR 2024, India’s largest young innovator initiative. The event features groundbreaking ideas from young innovators in technology, healthcare, sustainability, and education.",
+  link: "https://startupnews.fyi/event/navishkar-2024-jharkhand-july-29/",
+  imgSrc: "/media/nav.png",
+},
+{
+  title: "Top 10 Startups in Karnataka",
+  date: "Jun 29 , 2024",
+  description:
+    "Startup applications accepted at a state-level, spanning 20+ states, with 2000+ startups applying for the program. Shortlisted 8-10 startups for the finals of each state.",
+  link: "https://www.headstart.in/bharat-pitchathon",
+  imgSrc: "/media/bharat.png",
+},
+{
+  title: "Alchemyst AI Raises $300K to Drive AI-Powered Digital Workforce Solutions",
+  date: "Aug 2024",
+  description:
+    "Alchemyst AI secures $300K in pre-seed funding led by Inflection Point Ventures, aiming to revolutionize digital workforce solutions with AI-driven automation.",
+  link: "https://www.financialexpress.com/business/brandwagon-alchemyst-ai-raises-300k-to-drive-ai-powered-digital-workforce-solutions-3641283/",
+  imgSrc: "https://www.financialexpress.com/wp-content/uploads/2024/10/Untitled-design-2024-10-16T125134.055.png?w=1024",
+},
+{
+  title: "This Bengaluru Startup Secures $300K to Build AI Employees for Enterprises",
+  date: "Aug 2024",
+  description:
+    "Alchemyst AI aims to enhance enterprise efficiency by introducing digital employees that can seamlessly integrate into workspaces and drive sales and marketing automation.",
+  link: "https://analyticsindiamag.com/ai-origins-evolution/this-bengaluru-startup-secures-300k-to-build-ai-employees-for-enterprises/",
+  imgSrc: "/featuredin/aim_Rect.jpg",
+},
+{
+  title: "TableSprint and Alchemyst AI Raise Early-Stage Funding",
+  date: "Aug 2024",
+  description:
+    "Alchemyst AI and TableSprint, both early-stage startups, successfully raised significant funds to drive innovation in the AI and automation sectors.",
+  link: "https://www.entrepreneur.com/en-in/news-and-trends/tablesprint-and-alchemyst-ai-raise-early-stage-funding/481385",
+  imgSrc: "https://assets.entrepreneur.com/images/misc/1729084067_Photo-2024-10-16T183341.944.png?width=1000",
+},
+{
+  title: "Alchemyst AI Raises $300K in Pre-Seed Round Led by Inflection Point Ventures",
+  date: "Aug 2024",
+  description:
+    "Alchemyst AI is set to expand its AI-powered digital workforce solutions after securing $300K in pre-seed funding from Inflection Point Ventures.",
+  link: "https://www.marcamoney.com/alchemyst-ai-raises-300k-in-pre-seed-round-led-by-inflection-point-ventures/",
+  imgSrc: "https://www.marcamoney.com/wp-content/uploads/2024/10/Alchemyst-AI-raises-300K-in-pre-seed-round-led-by-Inflection-Point-Ventures.jpg",
+}
+
+]
+
+export default mediaTimes;
