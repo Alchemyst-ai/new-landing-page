b051c69daf6ec0c963e40cf6b8c6b0bb6003ee21
Debayan Pradhan <pradhandebayan@gmail.com>
2024-10-22 19:11:07 +0530

    [ADD] scss styles for integration styling

--- app/constants/integrations.ts
+++ app/constants/integrations.ts
@@ -1,17 +1,33 @@
-const integrations: string[] = [
-  "/integrations/hubspot.png",
-  "/integrations/gmail.webp",
-  "/integrations/swagger.png",
-  "/integrations/jira.png",
-  "/integrations/apollo.png",
-  "/integrations/salesforce.png",
-  "/integrations/slack.png",
-  "/integrations/zoho.png",
-  "/integrations/telegram.webp",
-  "/integrations/whatsapp.png",
-  "/integrations/freshdesk.png",
-  "/integrations/servicenow.png",
-  "/integrations/trello.png",
-];
+// const integrations: string[] = [
+//   "/integrations/hubspot.png",
+//   "/integrations/gmail.webp",
+//   "/integrations/swagger.png",
+//   "/integrations/jira.png",
+//   "/integrations/apollo.png",
+//   "/integrations/salesforce.png",
+//   "/integrations/slack.png",
+//   "/integrations/zoho.png",
+//   "/integrations/telegram.webp",
+//   "/integrations/whatsapp.png",
+//   "/integrations/freshdesk.png",
+//   "/integrations/servicenow.png",
+//   "/integrations/trello.png",
+// ];
+
+const integrations: {path: string, areaName : string}[] = [
+  { path: "/integrations/hubspot.png", areaName: "area1" },
+  { path: "/integrations/gmail.webp", areaName: "area2" },
+  { path: "/integrations/swagger.png", areaName: "area9" },
+  { path: "/integrations/jira.png", areaName: "area12" },
+  { path: "/integrations/apollo.png", areaName: "area5" },
+  { path: "/integrations/salesforce.png", areaName: "area6" },
+  { path: "/integrations/slack.png", areaName: "area3" },
+  { path: "/integrations/zoho.png", areaName: "area11" },
+  { path: "/integrations/telegram.webp", areaName: "area10" },
+  { path: "/integrations/whatsapp.png", areaName: "area4" },
+  { path: "/integrations/freshdesk.png", areaName: "area8" },
+  { path: "/integrations/servicenow.png", areaName: "area13" },
+  { path: "/integrations/trello.png", areaName: "area7" }
+]
 
 export default integrations;
--- components/home/Integrations.tsx
+++ components/home/Integrations.tsx
@@ -3,8 +3,13 @@ import integrations from "@/app/constants/integrations";
 import { motion } from "framer-motion";
 import Image from "next/image"; // Import for optimized images
 import GridLayout from "../GridLayout";
+import styles from './integration.module.scss'
+import useWindowDimensions from "@/hooks/useWindowDimentions";
 
 const Integrations: React.FC = () => {
+
+  const {width} = useWindowDimensions();
+
   return (
     <GridLayout>
       <motion.div
@@ -12,22 +17,26 @@ const Integrations: React.FC = () => {
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.5 }}
         viewport={{ once: false }}
-        className="relative flex flex-col items-center justify-center my-20"
+        className="relative flex flex-col items-center justify-center my-20 "
       >
-        <h2 className="text-[#21dbd8] mb-20 text-4xl md:text-5xl text-center mx-4">
-          {/* <span className="text-[#21dbd8]"> */}
+        <h2 className="text-[#21dbd8] mb-20 text-3xl sm:text-4xl md:text-5xl text-center mx-4">
           One-minute integrations
-          {/* </span> */}
-          {/* <br /> */}
         </h2>
-        <h3 className="mb-20 text-3xl md:text-3xl text-center mx-4">
+        <h3 className="mb-20 text-2xl sm:text-3xl md:text-3xl text-center mx-4">
           Alchemysts work wherever you work.
         </h3>
-        <div className="flex flex-wrap gap-28 w-2/3 justify-center items-center">
+        <div
+          className={`w-full flex justify-center items-center ${
+            width >= 640 ? 'flex-wrap gap-28 sm:w-2/3 ' : styles.integration__container
+          }`}
+         >
           {integrations.map((integration, index) => (
             <motion.div
               key={index}
-              className="h-16 w-auto"
+              style={{
+                gridArea: `${integration.areaName}`
+              }}
+              className="h-16 w-auto flex justify-center items-center"
               initial={{ opacity: 0, scale: 0.95, y: 20 }}
               whileInView={{ opacity: 1, scale: 1, y: 0 }}
               transition={{
@@ -38,7 +47,7 @@ const Integrations: React.FC = () => {
               }}
             >
               <Image
-                src={integration}
+                src={integration.path}
                 alt={`Integration ${index}`}
                 width={150}
                 height={150}
@@ -50,6 +59,9 @@ const Integrations: React.FC = () => {
             className="h-16 flex justify-center items-center w-auto text-gray-500 text-2xl"
             initial={{ opacity: 0, scale: 0.95, y: 20 }}
             whileInView={{ opacity: 1, scale: 1, y: 0 }}
+            style={{
+              gridArea:"area14"
+            }}
             transition={{
               duration: 0.6,
               delay: integrations.length * 0.1,
