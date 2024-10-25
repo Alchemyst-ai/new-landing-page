5f0efefba5225fc4c80f17b78ab2045e265637c2
Debayan Pradhan <pradhandebayan@gmail.com>
2024-10-25 22:04:16 +0530

    updated links

--- app/constants/team.ts
+++ app/constants/team.ts
@@ -5,7 +5,7 @@ export const team: Team[] = [
     name: "Uttaran Nayak",
     role: "Chief Executive Officer",
     image: "/team/uttaran.png",
-    linkedin: "https://www.linkedin.com/in/uttaran-nayak-1b1b3b1b1/",
+    linkedin: "https://www.linkedin.com/in/uttarannayak/",
     twitter: "https://twitter.com/uttaran_nayak",
     achievement:
       "Economic Times & Unstop Competitive Leader '23 | National Entrepreneurship Summit Winner '21 & '23 | Asia Pacific Global Hackathon Winner '23 | TEDx & Upcoming Josh Talks Speaker",
@@ -25,7 +25,7 @@ export const team: Team[] = [
     name: "Mukesh Kuiry",
     role: "Lead Cloud & Backend dev",
     image: "/team/mukesh.png",
-    linkedin: "https://www.linkedin.com/in/mukesh-kuiry/",
+    linkedin: "https://www.linkedin.com/in/mukeshkuiry/",
     twitter: "https://twitter.com/mukeshkuiry7",
     achievement:
       "Ex-Golang & Kubernetes Engineer @justk8s & @forge | Ex-Founding Developer @freeflow | Ex-Software Developer @chainrisk (prev UNSNARL) | Maintainer of Multiple Open Source Projects @clueless",
@@ -35,7 +35,7 @@ export const team: Team[] = [
     name: "Saptarshi Pani",
     role: "Chief Data Scientist",
     image: "/team/saptarshi.png",
-    linkedin: "https://www.linkedin.com/in/saptarshipani/",
+    linkedin: "https://www.linkedin.com/in/panisap/",
     twitter: "https://twitter.com/saptarshipani",
     achievement:
       "Richard E. Merwin International Scholar | 3 Research Papers in Machine Learning (International Journals) | 10x National Hackathon Winner | Economic Times & Unstop Competitive Leader '23",
@@ -45,7 +45,7 @@ export const team: Team[] = [
     name: "Debayan Pradhan",
     role: "Full Stack Web Developer",
     image: "/team/debayan.png",
-    linkedin: "https://www.linkedin.com/in/debayan-pradhan-1b1b3b1b1/",
+    linkedin: "https://www.linkedin.com/in/debayan-pradhan-b138641b4",
     twitter: "https://twitter.com/debayanpradhan",
     achievement:
       "Incubatee under Algorand Start-up Labs | National Runner-Up, Hack4Bengal 2023 | National Finalist, Smart India Hackathon 2023 | Google Developers Community Lead, KGEC Kalyani",
@@ -56,7 +56,7 @@ export const team: Team[] = [
     name: "Samarth Rawat",
     role: "Backend Engineer",
     image: "/team/samarth.png",
-    linkedin: "https://www.linkedin.com/in/samarth-rawat-1b1b3b1b1/",
+    linkedin: "https://www.linkedin.com/in/samarthrawat1/",
     twitter: "https://twitter.com/samarthrawat",
     achievement:
       "Finalist - GEHealthcare Hackathon | Regional Finalist - Solving for India Hackathon | Freelanced with Clients from the US and UK | Developed an Automated Reservation Bot with Python and Azure",
@@ -66,7 +66,7 @@ export const team: Team[] = [
     name: "Prithwijit Dey",
     role: "Founder's Office",
     image: "/team/prithwijit.png",
-    linkedin: "",
+    linkedin: "https://www.linkedin.com/in/prithwijitdey/",
     twitter: "",
     achievement:
       "Senior Consultant @ 180 Degrees Consulting | Ex- Consulting Intern @ENORD | Ex- BDA Intern @VentureLabs | Member @Google Developer Group | B.Tech BIT Mesra",
@@ -76,7 +76,7 @@ export const team: Team[] = [
     name: "Mehul Paul",
     role: "UI/UX Designer",
     image: "/team/mehul.png",
-    linkedin: "https://www.linkedin.com/in/mehul-paul-32896a242/",
+    linkedin: "www.linkedin.com/in/mehul-paul-32896a242/",
     twitter: "",
     achievement:
       "2022 NASA Industrial Design Finalist. 2023 NASA ANDC winner. Editorial Board, MANIT Joint Chief. UTHAAN Foundation NGO volunteer and designer. Giaaka WEB3 UX designer.",
@@ -86,7 +86,7 @@ export const team: Team[] = [
     name : "Arnab Sengupta",
     role : "Machine Learning Engineer",
     image : "/team/arnab.png",
-    linkedin : "https://www.linkedin.com/in/arnab-chakraborty-1b1b3b1b1/",
+    linkedin : "https://www.linkedin.com/in/mehul-paul-32896a242/",
     twitter : "https://twitter.com/arnabchakraborty",
     achievement : "Ex-Data Science Intern @Quantum Leap AI | Ex-Data Science Intern @Arakoo AI | Ex-Data Science Intern @AI4Bharat | Ex-Data Science Intern @Forge | Ex-Data Science Intern @JustK8s"
   },
