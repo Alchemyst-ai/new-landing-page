4f0fc1c8c63dd85ebe0a057c07b0e9c85f8fc822
Debayan Pradhan <pradhandebayan@gmail.com>
2024-10-28 20:09:23 +0530

    minor changes

--- components/home/Architecture.tsx
+++ components/home/Architecture.tsx
@@ -19,19 +19,19 @@ import 'slick-carousel/slick/slick.css';
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 4,
         },
       },
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
         },
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
         },
       }
     ],
