72a0e11161564d412310dae5d8b23918ff37b20c
vedantabanerjee <vedantabanerjee01@gmail.com>
2024-12-16 08:29:13 +0530

    [ADD]Landing Page - Hero Section

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,217 +1,141 @@
 "use client";
-import GridLayout from "@/components/GridLayout";
-import Announcement from "@/components/home/Announcement";
-import Button from "@/components/home/Button";
-import { motion, useScroll, useTransform } from "framer-motion";
-import Image from "next/image";
+import React from "react";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import Button from "./Button";
+import Image from "next/image";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import Typewriter from "typewriter-effect";
-import RotatingMaya from "./RotatingMaya";
 
-const Hero: React.FC = () => {
-  const { scrollY } = useScroll();
-  const y1 = useTransform(scrollY, [0, 500], [0, -200]);
-  const y2 = useTransform(scrollY, [0, 500], [0, -200]);
+function Hero() {
+    // Logos and settings for the carousal
+    const logos = [
+        "/enterprises/chainrisk.svg",
+        "/enterprises/forethought.png",
+        "/enterprises/plat.png",
+        "/enterprises/tesco.png",
+        "/enterprises/fourkites.webp",
+    ];
 
-  const [mounted, setMounted] = useState(false);
+    const settings = {
+        infinite: true,
+        speed: 5000,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        cssEase: "linear",
+        pauseOnHover: false,
+        arrows: false,
+        slidesToShow: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+            {
+                breakpoint: 375,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    };
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+    return (
+        <div className="w-full h-screen">
+            {/* Heading Section */}
+            <div className="mt-56 pl-10 md:pl-40">
+                <h1 className="text-6xl">Transform Business with </h1>
+                <h1 className="text-6xl">Next-Gen AI Employees ✨</h1>
+            </div>
 
-  const getRandomDelay = () => `${Math.random() * 5}s`;
+            {/* The description Section */}
+            <div className="mt-4 pl-10 md:pl-40">
+                <h2 className="text-gray-400">
+                    You can get fully set up in our platform in just three days and we
+                    offer
+                </h2>
+                <h2 className="text-gray-400">
+                    on hand support to ensure you will get maximum value out of
+                    outbound.
+                </h2>
 
-  const verticalLines = Array.from({ length: 10 }).map((_, index) => (
-    <motion.div
-      key={`v-${index}`}
-      className="h-screen absolute top-0 animated-line hidden sm:flex flex-col justify-end items-end"
-      style={{
-        right: `${20 + Math.random() * 20}%`,
-        animationDelay: getRandomDelay(),
-      }}
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{ duration: 1, delay: Math.random() * 2 }}
-    >
-      <div className="w-[1px] bg-gradient-to-b from-black via-gray-300 to-white h-64"></div>
-    </motion.div>
-  ));
+            </div>
 
-  const horizontalLines = Array.from({ length: 10 }).map((_, index) => (
-    <motion.div
-      key={`h-${index}`}
-      className="w-screen absolute right-0 animated-line-w hidden sm:flex flex-col justify-start items-start"
-      style={{
-        top: `${20 + Math.random() * 60}%`,
-        animationDelay: getRandomDelay(),
-      }}
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{ duration: 1, delay: Math.random() * 2 }}
-    >
-      <div className="h-[1px] w-32 bg-gradient-to-l from-black via-gray-300 to-white"></div>
-    </motion.div>
-  ));
+            {/* The button section */}
+            <div className="flex items-center space-x-4 mt-10 pl-10 md:pl-40">
+                <div className="flex items-center space-x-4">
+                    <Link href="https://calendly.com/uttaran-getalchemystai/30min">
+                        <Button variant="primary">Book a demo</Button>
+                    </Link>
 
-  const logos = [
-    "/enterprises/chainrisk.svg",
-    "/enterprises/forethought.png",
-    "/enterprises/plat.png",
-    "/enterprises/tesco.png",
-    "/enterprises/fourkites.webp",
-  ];
-
-  const settings = {
-    infinite: true,
-    speed: 5000,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 0,
-    cssEase: "linear",
-    pauseOnHover: false,
-    arrows: false,
-    slidesToShow: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-      {
-        breakpoint : 375,
-        settings: {
-          slidesToShow : 1
-        }
-      }
-    ],
-  };
-
-  if (!mounted) return null;
-
-  return (
-    <GridLayout>
-      <section className="relative z-20 flex h-full w-full items-center justify-center">
-        {verticalLines}
-        {horizontalLines}
-        <motion.div
-          className="relative mt-20 md:mt-40 w-full md:w-[76%]"
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: false }}
-          transition={{ duration: 0.5 }}
-          style={{ y: y1 }}
-        >
-          <div className="flex justify-between items-center">
-            <div className="w-full min-w-[90%] md:min-w-[60%] md:m-auto mx-5 my-2 md:w-1/2">
-              <header className="text-[clamp(1.8rem,4vw,5rem)] text-left  md:text-6xl  md:text-start font-bold leading-tight md:leading-[1.1]">
-                <div className="flex">
-                  <p className="text-blue-">Revolutionize&nbsp;</p>
-                  <Typewriter
-                    options={{
-                      strings: [
-                        `<span style="color: #ff9933;">Productivity</span>`,
-                        `<span style="color: #ff9933;">Business</span>`,
-                        `<span style="color: #ff9933;">Growth</span>`,
-                        `<span style="color: #ff9933;">Innovation</span>`,
-                        `<span style="color: #ff9933;">Automation</span>`,
-                      ],
-                      autoStart: true,
-                      loop: true,
-                    }}
-                  />
+                    <Link
+                        href="https://www.youtube.com/watch?v=m7qiEo9AXT8"
+                        target="_blank"
+                    >
+                        <Button
+                            variant="secondary"
+                            icon={
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-4 h-4"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M4.5 19.5l15-15m0 0H8.25m11.25 0V11.25"
+                                    />
+                                </svg>
+                            }
+                        >
+                            Watch Demo
+                        </Button>
+                    </Link>
                 </div>
-                with your Next-Gen AI Digital Employees
-              </header>
-              <p className="my-6 w-full md:w-2/3 text-xl font-medium text-left sm:text-center md:text-start text-gray-400">
-                Transform your business with the next ecosystem of gen-AI
-                digital employees built on Alchemyst&apos;s Infrastructure that
-                interacts with each other and form highly intelligent functional
-                components across enterprises.
-              </p>
-              <div className="flex flex-col md:flex-row gap-2">
-                  <Link
-                    href="https://calendly.com/uttaran-getalchemystai/30min"
-                    target="_blank noopener"
-                  >
-                    <Button variant={"primary"} className="w-full sm:w-full md:w-auto">
-                      Book a demo
-                    </Button>
-                  </Link>
-                <Link href="https://tripetto.app/run/60HWNW0WQN">
-                  <Button
-                    variant={"secondary"}
-                    className="w-full md:w-auto"
-                    icon={
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="size-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                        />
-                      </svg>
-                    }
-                  >
-                    Contact Us
-                  </Button>
-                </Link>
-              </div>
             </div>
-            <div className="w-1/2 -mr-40 -mt-10">
-              <RotatingMaya />
+
+            {/* The company image carousal section */}
+            <div className="flex flex-col justify-center items-center overflow-hidden mb-16 mt-16 bg-gradient-to-b from-[#000000] to-[#fe9833]">
+                <h2 className="text-xl sm:text-2xl mb-8 text-center">
+                    Best Enterprises and Startups trust us
+                </h2>
+                <div className="w-[100vw] sm:w-[100vw]">
+                    <Slider {...settings}>
+                        {logos.map((logo, index) => (
+                            <div key={index}>
+                                <Image
+                                    src={logo}
+                                    alt={`Company ${index + 1}`}
+                                    width={125}
+                                    height={50}
+                                    className="h-12 w-auto rounded"
+                                    priority
+                                />
+                            </div>
+                        ))}
+                    </Slider>
+                </div>
             </div>
-          </div>
-          <div className="w-full px-2">
-            <Announcement />
-          </div>
-          <div className="flex flex-col justify-center items-center overflow-hidden mb-16">
-            <h2 className="text-xl sm:text-2xl mb-8 text-center">
-              Best Enterprises and Startups trust us
-            </h2>
-            <div className="w-[70vw] sm:w-[60vw]">
-              <Slider {...settings}>
-                {logos.map((logo, index) => (
-                  <div key={index}>
-                    <Image
-                      src={logo}
-                      alt={`Company ${index + 1}`}
-                      width={150}
-                      height={50}
-                      className="h-12 w-auto rounded"
-                      priority
-                    />
-                  </div>
-                ))}
-              </Slider>
-            </div>
-          </div>
-        </motion.div>
-      </section>
-    </GridLayout>
-  );
-};
+        </div>
+    );
+}
 
 export default Hero;
---
+++ components/home/OldHero.tsx
@@ -0,0 +1,217 @@
+"use client";
+import GridLayout from "@/components/GridLayout";
+import Announcement from "@/components/home/Announcement";
+import Button from "@/components/home/Button";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import Typewriter from "typewriter-effect";
+import RotatingMaya from "./RotatingMaya";
+
+const Hero: React.FC = () => {
+  const { scrollY } = useScroll();
+  const y1 = useTransform(scrollY, [0, 500], [0, -200]);
+  const y2 = useTransform(scrollY, [0, 500], [0, -200]);
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const getRandomDelay = () => `${Math.random() * 5}s`;
+
+  const verticalLines = Array.from({ length: 10 }).map((_, index) => (
+    <motion.div
+      key={`v-${index}`}
+      className="h-screen absolute top-0 animated-line hidden sm:flex flex-col justify-end items-end"
+      style={{
+        right: `${20 + Math.random() * 20}%`,
+        animationDelay: getRandomDelay(),
+      }}
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1, delay: Math.random() * 2 }}
+    >
+      <div className="w-[1px] bg-gradient-to-b from-black via-gray-300 to-white h-64"></div>
+    </motion.div>
+  ));
+
+  const horizontalLines = Array.from({ length: 10 }).map((_, index) => (
+    <motion.div
+      key={`h-${index}`}
+      className="w-screen absolute right-0 animated-line-w hidden sm:flex flex-col justify-start items-start"
+      style={{
+        top: `${20 + Math.random() * 60}%`,
+        animationDelay: getRandomDelay(),
+      }}
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1, delay: Math.random() * 2 }}
+    >
+      <div className="h-[1px] w-32 bg-gradient-to-l from-black via-gray-300 to-white"></div>
+    </motion.div>
+  ));
+
+  const logos = [
+    "/enterprises/chainrisk.svg",
+    "/enterprises/forethought.png",
+    "/enterprises/plat.png",
+    "/enterprises/tesco.png",
+    "/enterprises/fourkites.webp",
+  ];
+
+  const settings = {
+    infinite: true,
+    speed: 5000,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: "linear",
+    pauseOnHover: false,
+    arrows: false,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint : 375,
+        settings: {
+          slidesToShow : 1
+        }
+      }
+    ],
+  };
+
+  if (!mounted) return null;
+
+  return (
+    <GridLayout>
+      <section className="relative z-20 flex h-full w-full items-center justify-center">
+        {verticalLines}
+        {horizontalLines}
+        <motion.div
+          className="relative mt-20 md:mt-40 w-full md:w-[76%]"
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{ duration: 0.5 }}
+          style={{ y: y1 }}
+        >
+          <div className="flex justify-between items-center">
+            <div className="w-full min-w-[90%] md:min-w-[60%] md:m-auto mx-5 my-2 md:w-1/2">
+              <header className="text-[clamp(1.8rem,4vw,5rem)] text-left  md:text-6xl  md:text-start font-bold leading-tight md:leading-[1.1]">
+                <div className="flex">
+                  <p className="text-blue-">Revolutionize&nbsp;</p>
+                  <Typewriter
+                    options={{
+                      strings: [
+                        `<span style="color: #ff9933;">Productivity</span>`,
+                        `<span style="color: #ff9933;">Business</span>`,
+                        `<span style="color: #ff9933;">Growth</span>`,
+                        `<span style="color: #ff9933;">Innovation</span>`,
+                        `<span style="color: #ff9933;">Automation</span>`,
+                      ],
+                      autoStart: true,
+                      loop: true,
+                    }}
+                  />
+                </div>
+                with your Next-Gen AI Digital Employees
+              </header>
+              <p className="my-6 w-full md:w-2/3 text-xl font-medium text-left sm:text-center md:text-start text-gray-400">
+                Transform your business with the next ecosystem of gen-AI
+                digital employees built on Alchemyst&apos;s Infrastructure that
+                interacts with each other and form highly intelligent functional
+                components across enterprises.
+              </p>
+              <div className="flex flex-col md:flex-row gap-2">
+                  <Link
+                    href="https://calendly.com/uttaran-getalchemystai/30min"
+                    target="_blank noopener"
+                  >
+                    <Button variant={"primary"} className="w-full sm:w-full md:w-auto">
+                      Book a demo
+                    </Button>
+                  </Link>
+                <Link href="https://tripetto.app/run/60HWNW0WQN">
+                  <Button
+                    variant={"secondary"}
+                    className="w-full md:w-auto"
+                    icon={
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                        />
+                      </svg>
+                    }
+                  >
+                    Contact Us
+                  </Button>
+                </Link>
+              </div>
+            </div>
+            <div className="w-1/2 -mr-40 -mt-10">
+              <RotatingMaya />
+            </div>
+          </div>
+          <div className="w-full px-2">
+            <Announcement />
+          </div>
+          <div className="flex flex-col justify-center items-center overflow-hidden mb-16">
+            <h2 className="text-xl sm:text-2xl mb-8 text-center">
+              Best Enterprises and Startups trust us
+            </h2>
+            <div className="w-[70vw] sm:w-[60vw]">
+              <Slider {...settings}>
+                {logos.map((logo, index) => (
+                  <div key={index}>
+                    <Image
+                      src={logo}
+                      alt={`Company ${index + 1}`}
+                      width={150}
+                      height={50}
+                      className="h-12 w-auto rounded"
+                      priority
+                    />
+                  </div>
+                ))}
+              </Slider>
+            </div>
+          </div>
+        </motion.div>
+      </section>
+    </GridLayout>
+  );
+};
+
+export default Hero;
