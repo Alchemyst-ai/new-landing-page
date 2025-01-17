16dc0b77501030cd131373f912aa5d00b62cf45b
vedantabanerjee <vedantabanerjee01@gmail.com>
2025-01-17 09:11:59 +0530

    [FIX]UI

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -56,12 +56,12 @@ function Hero() {
   };
 
   return (
-    <div className="relative w-full h-screen overflow-hidden">
+    <div className="relative w-full h-screen overflow-x-hidden overflow-y-auto">
       <video
         autoPlay
         loop
         muted
-        className="absolute inset-0 w-full h-full object-cover hidden md:block"
+        className="absolute inset-0 w-screen h-screen object-cover hidden md:block"
         preload="auto"
       >
         <source src="/media/landingpagevideo60_2.mp4" type="video/mp4" />
@@ -84,9 +84,9 @@ function Hero() {
         quality={100}
       />
 
-      <div className="relative z-10 flex flex-col h-full overflow-y-hidden">
+      <div className="relative z-10 flex flex-col max-h-screen max-w-screen overflow-hidden">
         {/* Heading Section for Desktop */}
-        <div className="mt-72 md:flex flex-col items-start pl-20 hidden font-bold">
+        <div className="mt-72 md:flex flex-col items-start ml-20 hidden font-bold">
           <h1 className="text-4xl md:text-6xl">Transform Your Sales with </h1>
           <h1 className="text-4xl md:text-6xl">AI-Driven Employees</h1>
         </div>
@@ -100,7 +100,7 @@ function Hero() {
 
         {/* The description Section */}
         <div className="mt-4">
-          <h2 className="text-lg text-gray-400 pl-10 pr-10 md:pl-20 md:pr-0 max-w-2xl text-center md:text-start font-semibold">
+          <h2 className="text-lg text-gray-400 pl-10 pr-10 md:pl-20 md:pr-0 max-w-2xl text-center md:text-start font-semibold overflow-hidden">
             Alchemyst AI empowers your team with an AI-powered sales platform
             that automates lead generation, outreach, and personalization -
             boosting productivity and driving growth.
@@ -108,7 +108,7 @@ function Hero() {
         </div>
 
         {/* The button section */}
-        <div className="flex items-center justify-center md:justify-start md:items-start md:pl-20 space-x-4 mt-14">
+        <div className="flex items-center justify-center md:justify-start md:items-start md:ml-20 space-x-4 mt-14 overflow-hidden">
           <div className="flex items-center space-x-4">
             <Link
               href="https://calendly.com/uttaran-getalchemystai/30min"
@@ -145,26 +145,25 @@ function Hero() {
             </Link>
           </div>
         </div>
-
-        {/* The company image carousal section */}
-        <div className="flex flex-col justify-center items-center overflow-hidden mt-32 md:mt-24">
-          <div className="absolute w-[100vw] sm:w-[100vw] mt-28 mb-20 md:bottom-0">
-            <Slider {...settings}>
-              {logos.map((logo, index) => (
-                <div key={index}>
-                  <Image
-                    src={logo || "/placeholder.svg"}
-                    alt={`Company ${index + 1}`}
-                    width={50}
-                    height={10}
-                    className="h-12 w-32 mt-20 md:h-12 md:w-52"
-                    loading="eager"
-                    quality={100}
-                  />
-                </div>
-              ))}
-            </Slider>
-          </div>
+      </div>
+      {/* The company image carousal section */}
+      <div className="flex flex-col justify-center items-center overflow-hidden mt-32 md:mt-24">
+        <div className="w-[100vw] sm:w-[100vw] mt-16 mb-20">
+          <Slider {...settings}>
+            {logos.map((logo, index) => (
+              <div key={index} className="max-w-[100vw]">
+                <Image
+                  src={logo || "/placeholder.svg"}
+                  alt={`Company ${index + 1}`}
+                  width={50}
+                  height={10}
+                  className="h-12 w-52"
+                  loading="eager"
+                  quality={100}
+                />
+              </div>
+            ))}
+          </Slider>
         </div>
       </div>
     </div>
@@ -173,6 +172,186 @@ function Hero() {
 
 export default Hero;
 
+// LAST SHIPped CODE WHICH I LIKED:
+// "use client";
+// import React from "react";
+// import Link from "next/link";
+// import Button from "./Button";
+// import Image from "next/image";
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
+// import { RetroGrid } from "../magicui/Retrogrid";
+
+// function Hero() {
+//   // Logos and settings for the carousal
+//   const logos = [
+//     "/enterprises/tesco.svg",
+//     "/enterprises/fourkites.svg",
+//     "/enterprises/decathlon.svg",
+//     "/enterprises/bajaj.svg",
+//     "/enterprises/fourthought.svg",
+//     "/enterprises/plat.svg",
+//   ];
+
+//   const settings = {
+//     infinite: true,
+//     speed: 5000,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 0,
+//     cssEase: "linear",
+//     pauseOnHover: false,
+//     arrows: false,
+//     slidesToShow: 4,
+//     responsive: [
+//       {
+//         breakpoint: 1024,
+//         settings: {
+//           slidesToShow: 3,
+//         },
+//       },
+//       {
+//         breakpoint: 600,
+//         settings: {
+//           slidesToShow: 2,
+//           centerMode: true,
+//           centerPadding: "10px",
+//         },
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           slidesToShow: 2,
+//           centerMode: true,
+//           centerPadding: "20px",
+//         },
+//       },
+//     ],
+//   };
+
+//   return (
+//     <div className="relative w-full h-screen overflow-hidden">
+//       <video
+//         autoPlay
+//         loop
+//         muted
+//         className="absolute inset-0 w-full h-full object-cover hidden md:block"
+//         preload="auto"
+//       >
+//         <source src="/media/landingpagevideo60_2.mp4" type="video/mp4" />
+//       </video>
+
+//       <div className="absolute inset-0 bg-gradient-to-t from-[#0B0B09] via-transparent to-transparent opacity-100" />
+
+//       <RetroGrid className="md:hidden block" />
+
+//       <div className="w-2/3 absolute inset-0 bg-gradient-to-l from-transparent via-[#0B0B09] to-[#0B0B09] opacity-90 hidden md:block" />
+
+//       <Image
+//         src="/media/topcard.svg"
+//         alt="Top GenAI Startup Card"
+//         width={50}
+//         height={50}
+//         className="h-14 md:h-20 w-auto rounded-3xl top-24 left-16 md:top-48 md:left-14 absolute"
+//         loading="eager"
+//         priority
+//         quality={100}
+//       />
+
+//       <div className="relative z-10 flex flex-col h-full overflow-y-hidden">
+//         {/* Heading Section for Desktop */}
+//         <div className="mt-72 md:flex flex-col items-start pl-20 hidden font-bold">
+//           <h1 className="text-4xl md:text-6xl">Transform Your Sales with </h1>
+//           <h1 className="text-4xl md:text-6xl">AI-Driven Employees</h1>
+//         </div>
+
+//         {/* Heading Section for Mobile */}
+//         <div className="mt-40 md:hidden flex items-center justify-center self-center text-center font-bold">
+//           <h1 className="text-5xl pl-2 pr-2 md:pl-0 md:pr-0 md:text-7xl">
+//             Transform Your Sales with AI-Driven Employees
+//           </h1>
+//         </div>
+
+//         {/* The description Section */}
+//         <div className="mt-4">
+//           <h2 className="text-lg text-gray-400 pl-10 pr-10 md:pl-20 md:pr-0 max-w-2xl text-center md:text-start font-semibold">
+//             Alchemyst AI empowers your team with an AI-powered sales platform
+//             that automates lead generation, outreach, and personalization -
+//             boosting productivity and driving growth.
+//           </h2>
+//         </div>
+
+//         {/* The button section */}
+//         <div className="flex items-center justify-center md:justify-start md:items-start md:pl-20 space-x-4 mt-14">
+//           <div className="flex items-center space-x-4">
+//             <Link
+//               href="https://calendly.com/uttaran-getalchemystai/30min"
+//               target="_blank"
+//             >
+//               <Button variant="primary">Book a demo</Button>
+//             </Link>
+
+//             <Link
+//               href="https://www.youtube.com/watch?v=m7qiEo9AXT8"
+//               target="_blank"
+//             >
+//               <Button
+//                 variant="secondary"
+//                 icon={
+//                   <svg
+//                     xmlns="http://www.w3.org/2000/svg"
+//                     fill="none"
+//                     viewBox="0 0 24 24"
+//                     strokeWidth={1.5}
+//                     stroke="currentColor"
+//                     className="w-4 h-4"
+//                   >
+//                     <path
+//                       strokeLinecap="round"
+//                       strokeLinejoin="round"
+//                       d="M4.5 19.5l15-15m0 0H8.25m11.25 0V11.25"
+//                     />
+//                   </svg>
+//                 }
+//               >
+//                 Watch Demo
+//               </Button>
+//             </Link>
+//           </div>
+//         </div>
+
+//         {/* The company image carousal section */}
+//         <div className="flex flex-col justify-center items-center overflow-hidden mt-32 md:mt-24">
+//           <div className="absolute w-[100vw] sm:w-[100vw] mt-28 mb-20 md:bottom-0">
+//             <Slider {...settings}>
+//               {logos.map((logo, index) => (
+//                 <div key={index}>
+//                   <Image
+//                     src={logo || "/placeholder.svg"}
+//                     alt={`Company ${index + 1}`}
+//                     width={50}
+//                     height={10}
+//                     className="h-12 w-32 mt-20 md:h-12 md:w-52"
+//                     loading="eager"
+//                     quality={100}
+//                   />
+//                 </div>
+//               ))}
+//             </Slider>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Hero;
+
+
+
+
+// OLD DAFAULT CODE
 // "use client";
 // import React from "react";
 // import Link from "next/link";
