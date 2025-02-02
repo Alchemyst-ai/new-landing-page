4c28f6d62161d8b6fb61ce5e71223514f1cbe5c2
Anuran Roy <anuranroy02@gmail.com>
2025-02-02 18:33:02 +0530

    [FIX] API fetch error

--- components/blogs/blog-content-data.ts
+++ components/blogs/blog-content-data.ts
@@ -2,10 +2,7 @@ import { BlogContent } from "../types/blog-content";
 
 export async function fetchBlogs(): Promise<BlogContent[]> {
   try {
-    const apiUrl =
-      process.env.NEXT_PUBLIC_API_URL || "http://localhost:4163/api/blogs";
-
-    const response = await fetch(apiUrl);
+    const response = await fetch("/api/blogs");
     if (!response.ok) throw new Error("Failed to fetch blogs");
 
     return await response.json();
