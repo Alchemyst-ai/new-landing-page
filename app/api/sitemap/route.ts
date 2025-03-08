6d70d44ee73f36d84f54ad9a48067def6b0c929d
Anuran Roy <anuranroy02@gmail.com>
2025-03-08 18:22:20 +0530

    [FIX] Fixed sitemap fields

--- app/api/sitemap/route.ts
+++ app/api/sitemap/route.ts
@@ -13,14 +13,14 @@ export const GET = async (req: NextRequest) => {
 
     const rawBlogs: ProcessedFile[] = await apiResults.json();
 
-    const blogs = rawBlogs.map(blog => {
-        const { id, title, image, draftStatus, ...blogObject } = {
-            url: `${baseUrl}${blog.redirectLink}`,
-            ...blog,
+    const blogs = rawBlogs.map(blog => ({
+        url: {
+            loc: `${baseUrl}${blog.redirectLink}`,
+            lastmod: blog.lastModified,
+            changefreq: 'weekly',
+            priority: '0.8'
         }
-
-        return blogObject
-    })
+    }))
 
     const xmlObject = { urlset: blogs }
 
