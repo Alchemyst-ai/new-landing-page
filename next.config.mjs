acfef9a0f1b23e28b67fd522a0113814d38c6d34
Debayan Pradhan <pradhandebayan@gmail.com>
2025-10-25 17:44:18 +0530

    [UPDATE] image url updated in config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const nextConfig = {
     remotePatterns: [
       { hostname: "localhost" },
       { hostname: "images.unsplash.com" },
-      { hostname: "charming-crown-5c60ef85ae.strapiapp.com"}
+      { hostname: "charming-crown-5c60ef85ae.strapiapp.com"},
+      { hostname: "charming-crown-5c60ef85ae.media.strapiapp.com" },
     ],
   },
   transpilePackages: ["geist"],
