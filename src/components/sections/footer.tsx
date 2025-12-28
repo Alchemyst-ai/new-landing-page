e58e2761f0d7f980b4292bab643e37f833ee87cd
Anuran Roy <anuranroy02@gmail.com>
2025-12-28 17:23:29 +0530

    [FIX, MODIFY] Fix footer, modified sitemap fetching

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -1,7 +1,7 @@
-import { Mailm "lucidelucide-react
-import Imageom "nexnext/image
-import Linkfrom "next/llink
-import { siteConfig }teConfi@/libfconfig"@/lib/config";
+import { siteConfig } from "@/lib/config";
+import { Mail } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export function Footer() {
   const footerLinks = {
