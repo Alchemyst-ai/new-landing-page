d4e6dbfb2d56f285153b0e97327e72954d3071b7
Debayan Pradhan <pradhandebayan@gmail.com>
2026-01-05 21:19:34 +0530

    [UPDATE] minor hover update

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -338,7 +338,7 @@ export function Header() {
                         window.dispatchEvent(new Event("open-founder-cal"));
                         setActiveMenu(null);
                       }}
-                      className="block w-full text-left px-4 py-2 rounded-lg text-md font-medium"
+                      className="block w-full text-left px-4 py-2 rounded-lg text-md font-medium hover:text-inherit"
                     >
                       <div className="text-md">Talk to Founders</div>
 					{/* <div>Chat with context. No signup required.</div> */}
